e63213b260c42b467de5ff7af833d9fb1f6d96ee
RichT-PT <diverdwn06@outlook.com>
2023-01-16 17:55:40 -0600

    promise code

--- testerFile.js
+++ testerFile.js
@@ -1,1 +1,16 @@
-# TesterFolder4
+
+let p = new promise ((resolve, reject) => {
+    let a = 17
+    if(a >= 16){
+      resolve("Verified")
+    }
+    else{
+      reject("Under Aged!")
+    }
+  })
+  p.then((Message) => {
+    console.log("Age is acceptible to drive" + message)
+  })
+  .catch((message) => {
+    console.log("Must be 16 to drive" + message)
+  })
